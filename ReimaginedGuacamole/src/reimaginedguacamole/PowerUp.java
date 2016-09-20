b3a97f2707c06190e83b4ac1be02fe91b837202d
MvG013 <marcvangool@hotmail.com>
2016-09-20 13:49:24 +0200

    nog 2 blijkbaar

---
+++ ReimaginedGuacamole/src/reimaginedguacamole/PowerUp.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reimaginedguacamole;
+
+/**
+ *
+ * @author Marc
+ */
+public class PowerUp {
+    
+    private String description;
+    private String name;
+
+    public PowerUp(String description, String name) {
+        this.description = description;
+        this.name = name;
+    }
+        
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+
+    
+
+    
+    
+}
