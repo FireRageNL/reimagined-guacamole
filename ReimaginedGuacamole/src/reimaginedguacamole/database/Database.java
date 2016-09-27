f22602572883e3384324530abafef27818e5ac45
Jormungandrrr <j.verstijlen@gmail.com>
2016-09-27 11:31:53 +0200

    Database connection
    -connection
    -statement for insert

--- ReimaginedGuacamole/src/reimaginedguacamole/database/Database.java
+++ ReimaginedGuacamole/src/reimaginedguacamole/database/Database.java
@@ -5,13 +5,22 @@
  */
 package reimaginedguacamole.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *Class for connecting to the database
  * @author daan
  */
 
-public class Database {
+public class  Database {
     private String connectionString;
+    private Connection conn;
     
     /**
      * loads the properties from a file
@@ -22,6 +31,57 @@ public class Database {
     
     public void setConnectionString(String connectionString){
         this.connectionString = connectionString;
+    }
+    
+    public void Insert(String table, Map<String, String> data){
+        try {
+            int columnCount = 0;
+            String columns = "";
+            String values = "";
+            
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+            columns += (entry.getKey()+",");
+            columnCount++;
+            }
+            for (int i = 0; i < columnCount; i++) {
+                values += "?,";
+            }
+            
+            values = values.substring(0,values.length()-1);
+            columns = columns.substring(0,values.length()-1);
+
+            String statement = "INSERT INTO Drawing ("+ columns +") VALUES ("+values+")";
+            PreparedStatement pst = conn.prepareStatement(statement);
+            System.out.println(statement);
+            
+            
+            //pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void Update(String table, Map<String, String> data){
+        
+    }
+    
+    public String ReadString()
+    {
+        return "";
+    }
+    
+    public void initConnection(){
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://daantuller.nl:3306/Guacamole","Guacamole","Guacamole01");
+            System.out.println("Connection ok.");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void closeConnection() throws SQLException{
+        conn.close();
     }
     
 }
