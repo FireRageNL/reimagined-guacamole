a0cde207c2ed2ad4dd59bb03895fd293c60ad0fc
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2016-10-11 12:33:26 +0200

    FXML fix
    wooo

--- ReimaginedGuacamole/src/reimaginedguacamole/profile/Login.java
+++ ReimaginedGuacamole/src/reimaginedguacamole/profile/Login.java
@@ -6,9 +6,6 @@
 package reimaginedguacamole.profile;
 
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import reimaginedguacamole.database.ProfileDB;
 import reimaginedguacamole.tooling.Hashing;
 
