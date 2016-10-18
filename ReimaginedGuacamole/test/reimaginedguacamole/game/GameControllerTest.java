7636263fc65b0aa03a46e3778a52d6c7e0c8ced0
MvG013 <marcvangool@hotmail.com>
2016-10-18 15:29:40 +0200

    gamecontrollertest

---
+++ ReimaginedGuacamole/test/reimaginedguacamole/game/GameControllerTest.java
@@ -0,0 +1,266 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reimaginedguacamole.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import reimaginedguacamole.profile.Profile;
+
+/**
+ *
+ * @author Marc
+ */
+public class GameControllerTest {
+    
+    public GameControllerTest() {
+    }
+    
+
+    /**
+     * Test of startNextRound method, of class GameController.
+     */
+    @Test
+    public void testStartNextRound() {
+        System.out.println("startNextRound");
+        GameController instance = new GameController(5,10);
+        instance.startNextRound();
+    }
+
+    /**
+     * Test of endGame method, of class GameController.
+     */
+    @Test
+    public void testEndGame() {
+        System.out.println("endGame");
+        Profile user = new Profile("test@email.com","test","Testie",2,10,20);     
+        GameController instance = new GameController(5,10);
+        instance.endGame(user);
+    }
+
+    /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategoryHistory() {
+        System.out.println("chooseCategory");
+        double wheel = 10.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.History;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+        /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategoryGames() {
+        System.out.println("chooseCategory");
+        double wheel = 60.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.Games;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+            /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategoryMusic() {
+        System.out.println("chooseCategory");
+        double wheel = 110.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.Music;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+                /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategoryScience() {
+        System.out.println("chooseCategory");
+        double wheel = 170.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.Science;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategoryArt() {
+        System.out.println("chooseCategory");
+        double wheel = 220.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.Art;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+         /**
+     * Test of chooseCategory method, of class GameController.
+     */
+    @Test
+    public void testChooseCategorySport() {
+        System.out.println("chooseCategory");
+        double wheel = 320.0;
+        GameController instance = new GameController(5,10);
+        Category expResult = Category.Sport;
+        Category result = instance.chooseCategory(wheel);
+        assertEquals(expResult, result);
+    }
+    
+
+    /**
+     * Test of getCurrentRound method, of class GameController.
+     */
+    @Test
+    public void testGetCurrentRound() {
+        System.out.println("getCurrentRound");
+        GameController instance = new GameController(5,10);
+        Round expResult = null;
+        Round result = instance.getCurrentRound();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of giveRoundQuestion method, of class GameController.
+     */
+    @Test
+    public void testGiveRoundQuestion() {
+        System.out.println("giveRoundQuestion");
+        Category category = Category.History;
+        GameController instance = new GameController(5,10);
+        instance.giveRoundQuestion(category);
+    }
+
+    /**
+     * Test of getGame method, of class GameController.
+     */
+    @Test
+    public void testGetGame() {
+        System.out.println("getGame");
+        GameController instance = new GameController(5,10);
+        int expResult = new Game(10,5).getAmountOfRounds();
+        int result = instance.getGame().getAmountOfRounds();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGameState method, of class GameController.
+     */
+    @Test
+    public void testGetGameState() {
+        System.out.println("getGameState");
+        GameController instance = new GameController(5,10);
+        instance.setGameState(GameState.GameRunning);
+        GameState expResult = GameState.GameRunning;
+        GameState result = instance.getGameState();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setGameState method, of class GameController.
+     */
+    @Test
+    public void testSetGameState() {
+        System.out.println("setGameState");
+        GameState gameState = GameState.GameRunning;
+        GameController instance = new GameController(5,10);
+        instance.setGameState(gameState);
+    }
+
+    /**
+     * Test of getCurrentAnswer method, of class GameController.
+     */
+    @Test
+    public void testGetCurrentAnswer() {
+        System.out.println("getCurrentAnswer");
+        GameController instance = new GameController(5,10);
+        instance.setCurrentAnswer(1);
+        int expResult = 1;
+        int result = instance.getCurrentAnswer();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setCurrentAnswer method, of class GameController.
+     */
+    @Test
+    public void testSetCurrentAnswer() {
+        System.out.println("setCurrentAnswer");
+        int currentAnswer = 1;
+        GameController instance = new GameController(5,10);
+        instance.setCurrentAnswer(currentAnswer);
+    }
+
+    /**
+     * Test of getCorrectAnswer method, of class GameController.
+     */
+    @Test
+    public void testGetCorrectAnswer() {
+        System.out.println("getCorrectAnswer");
+        GameController instance = new GameController(5,10);
+        instance.startNextRound();
+        instance.giveRoundQuestion(Category.History);
+        int expResult = instance.getCurrentRound().getQuestion().getCorrectAnswer();
+        int result = instance.getCorrectAnswer();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCurrentScore method, of class GameController.
+     */
+    @Test
+    public void testGetCurrentScore() {
+        System.out.println("getCurrentScore");
+        GameController instance = new GameController(5,10);
+        int expResult = 0;
+        int result = instance.getCurrentScore();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCurrentRoundIndex method, of class GameController.
+     */
+    @Test
+    public void testGetCurrentRoundIndex() {
+        System.out.println("getCurrentRoundIndex");
+        GameController instance = new GameController(5,10);
+        instance.startNextRound();
+        int expResult = 0;
+        int result = instance.getCurrentRoundIndex();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of checkAnswer method, of class GameController.
+     */
+    @Test
+    public void testCheckAnswer() {
+        System.out.println("checkAnswer");
+        Profile profile = new Profile("IkHak@live.nl","Jorrit","HardcoreJorrit",46,0,0);     
+        double timeTaken = 5.0;
+        GameController instance = new GameController(5,10);
+        boolean expResult = false;
+        instance.startNextRound();
+        instance.giveRoundQuestion(Category.History);
+        instance.setCurrentAnswer(1);
+        boolean result = instance.checkAnswer(profile, timeTaken);
+        assertEquals(expResult, result);
+    }
+}
