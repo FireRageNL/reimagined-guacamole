df3b3795333509c77eb205d926c52aabb301c68e
Tuller,Daan D <daan.tuller@student.fontys.nl>
2016-11-30 13:04:51 +0100

    Game Room class added

---
+++ ReimaginedGuacamoleMS/src/reimaginedguacamole/game/GameRoom.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reimaginedguacamole.game;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import reimaginedguacamole.profile.ChatServer;
+import reimaginedguacamole.profile.IProfile;
+
+/**
+ *
+ * @author daan
+ */
+public class GameRoom extends UnicastRemoteObject implements IGameRoom{
+    //private IGameController
+    
+    public GameRoom() throws RemoteException{
+        
+    }
+    
+    private List<IProfile> players;
+    private ChatServer chatserver;
+
+    
+    
+    @Override
+    public int getNrOfPlayers() throws RemoteException {
+        return players.size();
+    }
+
+    @Override
+    public void joinRoom(IProfile profile) throws RemoteException {
+        players.add(profile);
+    }
+
+    @Override
+    public void leaveRoom(IProfile profile) throws RemoteException {
+        players.remove(profile);
+    }
+
+    @Override
+    public List<String> getNicknames() throws RemoteException {
+        List<String> nicks = new ArrayList();
+        for(IProfile p : players){
+            nicks.add(p.getNickname());
+        }
+       return nicks;
+    }
+    
+    
+}
---
+++ ReimaginedGuacamoleMS/src/reimaginedguacamole/game/IGameRoom.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reimaginedguacamole.game;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import reimaginedguacamole.profile.ChatServer;
+import reimaginedguacamole.profile.IProfile;
+import reimaginedguacamole.profile.Profile;
+
+/**
+ *
+ * @author daan
+ */
+public interface IGameRoom extends Remote {
+    
+    public int getNrOfPlayers() throws RemoteException;
+    
+    public void joinRoom(IProfile profile) throws RemoteException;
+    
+    public void leaveRoom(IProfile profile) throws RemoteException;
+    
+    public List<String> getNicknames() throws RemoteException;
+    
+}
